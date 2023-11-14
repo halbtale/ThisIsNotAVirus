dc9b31a7a464d68d0c31df3436446b20eefabed7
Alberto <42894888+Nenino2@users.noreply.github.com>
2023-11-14 08:38:27 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## This is not a virus
+Ever wondered how can you write files in Java? Feel free to try this code out!
+
+### Luck
+You have a 50% chance that the program will not harm your PC
+
+### Important
+Don't read the source code otherwise you don't have a surprise
---
+++ ThisIsNotAVirus.java
@@ -0,0 +1,25 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+class ThisIsNotAVirus {
+    public static void main(String[] args) throws FileNotFoundException {
+        boolean isVirus = Math.random() > 0.5;
+        if (isVirus) {
+            for (int i = 0; i < 5; i++) {
+                PrintWriter out = new PrintWriter("ThisIsNotAVirus" + i + ".java");
+                FileReader reader = new FileReader("ThisIsNotAVirus.java");
+                Scanner in = new Scanner(reader);
+                while (in.hasNextLine()) {
+                    String line = in.nextLine();
+                    out.println(line);
+                }
+                in.close();
+                out.close();
+            }
+        } else {
+            System.out.println("I told you I am not a virus! :(");
+        }
+    }
+}
